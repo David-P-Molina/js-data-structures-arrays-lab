49aafe68aa4ab4eec4e4b7f99106a9dc0b7a2f00
David Moina <davemolina777@gmail.com>
2021-04-29 13:49:29 -0500

    commit tree 40d8e6923a9d2af3dde5d53cc619c9e9291c5c0d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+function destructivelyAppendDriver(name){
+    drivers.push(name)
+}
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+function destructivelyRemoveLastDriver(name){
+    drivers.pop(name)
+}
+function destructivelyRemoveFirstDriver(name){
+    drivers.shift(name)
+}
+function appendDriver(name){
+    return [...drivers, name]
+    
+}
+function prependDriver(name){
+    return [name, ...drivers]
+    
+}
+let drivers = ["Milo", "Otis", "Garfield"]
+function removeFirstDriver(){
+    return drivers.slice(1);
+}
+function removeLastDriver(){
+    return drivers.slice(0, drivers.length - 1)
+}
